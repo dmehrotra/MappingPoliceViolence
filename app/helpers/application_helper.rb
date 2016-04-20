ee8ba1a02e34a0243119a8a2f406a1ae3dface6a
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2016-04-20 17:20:04 -0400

    authentication

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,8 @@
 module ApplicationHelper
 	 def check_admin?
-        # here is a method to check if we are logged in
-        # if not...redirect
         unless current_user
             redirect_to "/login"
             return
         end
-
     end
 end

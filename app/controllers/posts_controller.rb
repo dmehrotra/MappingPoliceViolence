fb2e471229dbbd27aacc90e4f8db88609948c8a3
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2016-05-04 13:17:27 -0400

    front end

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,15 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    sim = []
+    @similar = []
+    @post.tags.each do |t|
+    	t.posts.each do |p|
+			if p.id != @post.id
+				sim << p
+			end
+    	end
+    end
+    @similar = sim.uniq{|x| x.id}
   end
 end

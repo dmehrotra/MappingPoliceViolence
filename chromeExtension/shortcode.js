e57bfab7114824a367ced99ba66342684679b99a
mathuramg <mmg9291@gmail.com>
2016-05-06 12:14:32 -0400

    make tweet button custom

--- chromeExtension/shortcode.js
+++ chromeExtension/shortcode.js
@@ -1,5 +1,5 @@
 var postTags = [];
-//can this be recieved by an api call ?
+
 var JSONApiData;
 makeAjaxRequest();
 
@@ -12,7 +12,6 @@ function makeAjaxRequest()
 		success: function(result){
 			window.console.log('success');
 			window.console.log(result);
-	// debugger;
 			var data = JSON.parse(result["posts"]);
 			// debugger;
 			JSONApiData = data;
@@ -27,87 +26,6 @@ function getPostTagArray(){
 	}
 }
 
-var JSONData =
-{
-	"banner": "Police killed at least 346 black people in the U.S. in 2015.",
-	"data":
-	[
-		{
-			"id":1,
-		  "description": "",
-		  "title": "Police are killing black people at persistently high rates.",
-		  "imgUrl": "http://static1.squarespace.com/static/54ecf211e4b0ed744420c5b6/t/	57114501b654f9ca26514ef6/1460749577385/PoliceKillingsTrendline.png?format=1000w",
-		  "tags": [
-		    "policeKillings",
-		    "trend"
-		  ],
-		  "questions" : [
-		    "question 1",
-		    "question 2"
-		  ]
-		},
-		{
-			"id":2,
-		  "description": "",
-		  "title": "Black people are most likely to be killed by police.",
-		  "imgUrl": "http://static1.squarespace.com/static/54ecf211e4b0ed744420c5b6/t/5695519969492ee091ce55a1/1452626355687/blackpeoplemorelikelytobekilled.png?format=1000w",
-		  "tags": [
-		    "policeKillings",
-		    "likelihood"
-		  ],
-		  "questions" : [
-		    "question 2",
-		    "question 3"
-		  ]
-		},
-		{
-			"id":3,
-		  "description": "",
-		  "title": "Where you live matters.",
-		  "imgUrl": "http://static1.squarespace.com/static/54ecf211e4b0ed744420c5b6/t/570e8e0c356fb0af9cde3faf/1460571675358/?format=1000w",
-		  "tags": [
-		    "policeKillings",
-		    "location",
-				"cities"
-		  ],
-		  "questions" : [
-		    "question 4",
-		    "question 5"
-		  ]
-		},
-		{
-			"id":4,
-		  "description": "",
-		  "title": "Where you live matters.",
-		  "imgUrl": "http://static1.squarespace.com/static/54ecf211e4b0ed744420c5b6/t/570e8e0c356fb0af9cde3faf/1460571675358/?format=1000w",
-		  "tags": [
-		    "policeKillings",
-		    "location",
-				"cities"
-		  ],
-		  "questions" : [
-		    "question 4",
-		    "question 5"
-		  ]
-		},
-		{
-			"id":5,
-		  "description": "",
-		  "title": "Where you live matters.",
-		  "imgUrl": "http://static1.squarespace.com/static/54ecf211e4b0ed744420c5b6/t/570e8e0c356fb0af9cde3faf/1460571675358/?format=1000w",
-		  "tags": [
-		    "policeKillings",
-		    "location",
-				"cities"
-		  ],
-		  "questions" : [
-		    "question 4",
-		    "question 5"
-		  ]
-		}
-	]
-};
-
 $('.tweet-box.rich-editor').keyup(function() {
   var that = this;
 	window.console.log( that.innerHTML );
@@ -121,11 +39,7 @@ $('.tweet-box.rich-editor').keyup(function() {
     insertPost(postNumber,'.tweet-box.rich-editor');
     var replaceText = $(that).text().replace(postTag,'');
     $(that).text(replaceText);
-
-
-
   }
-
 });
 
 function findMatchingWords(t, s) {
@@ -147,6 +61,5 @@ function insertPost(postNumber,container)
   textContainer.html(postUrl + tagString);
 
   $(container).append(textContainer);
-
 
 }
